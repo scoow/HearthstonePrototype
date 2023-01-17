be9401b29b90429e2738e2ef18d934ae53363b3c
kimbo-kumbo <92639132+kimbo-kumbo@users.noreply.github.com>
2023-01-17 23:11:34 +0300

    ADD (SO):
    добавил коллекции SO

--- Assets/Scripts/PageBook.cs
+++ Assets/Scripts/PageBook.cs
@@ -1,11 +1,17 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Hearthstone
 {
     public class PageBook : MonoBehaviour
     {
-        public GameObject[] _cardsInPageBook;
-        public Sprite _spriteCard;
+        public List<CardSO> _1manaCostCard;
+        public List<CardSO> _2manaCostCard;
+        public List<CardSO> _3manaCostCard;
+        public List<CardSO> _4manaCostCard;
+        public List<CardSO> _5manaCostCard;
+        public List<CardSO> _6manaCostCard;
+        public List<CardSO> _7manaCostCard;        
 
     }
 }
