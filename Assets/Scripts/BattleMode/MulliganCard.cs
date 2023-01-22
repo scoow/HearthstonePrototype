bb4ce2362a6c54b665778c62c403a803a4530871
scoow <85620078+scoow@users.noreply.github.com>
2023-01-22 20:48:00 +0300

    Сделаны две стадии муллигана
    1. Вылет карт из деки
    2. ---
    3. Убирание карт в деку

--- Assets/Scripts/BattleMode/MulliganCard.cs
+++ Assets/Scripts/BattleMode/MulliganCard.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using Cysharp.Threading.Tasks;
+using System;
 
 namespace Hearthstone
 {
--- Assets/Scripts/BattleMode/MulliganManager.cs
+++ Assets/Scripts/BattleMode/MulliganManager.cs
@@ -12,19 +12,27 @@ namespace Hearthstone
         [SerializeField]
         private float _time = 2f;
 
+        [SerializeField]
+        private GameObject _playerDeck;
+
+
         private List<MulliganCardPosition> _mulliganCardsPositions;//якори дл€ вылетающих карт
         private List<MulliganCard> _mulliganCards;//—ами карты
-        private void Start()
+        private async void Start()
         {
             _mulliganCardsPositions = new List<MulliganCardPosition>();
             _mulliganCardsPositions = FindObjectsOfType<MulliganCardPosition>().ToList();
+            _mulliganCardsPositions.Sort((c1, c2) => string.Compare(c1.gameObject.name, c2.gameObject.name));//todo улучшить сортировку
 
             _mulliganCards = new List<MulliganCard>();
             _mulliganCards = FindObjectsOfType<MulliganCard>().ToList();
+            _mulliganCards.Sort((c1, c2) => string.Compare(c1.gameObject.name, c2.gameObject.name));
             foreach (var card in _mulliganCards)
                 card.gameObject.SetActive(true);
 
             MulliganStage1();
+            await UniTask.Delay(TimeSpan.FromSeconds(5));
+            MulliganStage3();
         }
 
         private async void MulliganStage1()
@@ -39,14 +47,16 @@ namespace Hearthstone
                 i++;
             }
         }
-        private void MulliganStage3()
+
+        private async void MulliganStage3()
         {
             MulliganCard _card;
             int i = 0;
             foreach (MulliganCardPosition position in _mulliganCardsPositions)
             {
                 _card = _mulliganCards[i];
-                StartCoroutine(_card.StartMulligan(_card.transform.position, position.transform.position, _card.transform.rotation, position.transform.rotation, _time));
+                _ = _card.StartMulliganAsync(position.transform.position, _playerDeck.transform.position, position.transform.rotation, _playerDeck.transform.rotation, _time);
+                await UniTask.Delay(TimeSpan.FromSeconds(0.5));
                 i++;
             }
         }
