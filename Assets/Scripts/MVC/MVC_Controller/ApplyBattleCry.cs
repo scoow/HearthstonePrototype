1315ab63425503666027b5f51df266142be36533
scoow <85620078+scoow@users.noreply.github.com>
2023-03-19 19:27:02 +0300

    Починил
    Псаря и синдорайку
    Но появился новый баг - они иногда могут атаковать сразу после клича. И не выключается курсор

--- Assets/Scripts/MVC/MVC_Controller/ApplyBattleCry.cs
+++ Assets/Scripts/MVC/MVC_Controller/ApplyBattleCry.cs
@@ -11,16 +11,16 @@ namespace Hearthstone
         private BattleCry_Controller _battleCryController;
         private BattleModeCard_View _battleModeCard_View;
         private EventEffect_Controller _eventEffectController;
-        private SingleEffect_Controller _singleEffectController;        
+        private SingleEffect_Controller _singleEffectController;
         private Card_Model _card_Model;
         private Card_Controller _card_Controller;
         [Inject]
-        private PageBook_Model _pageBook_Model;        
+        private PageBook_Model _pageBook_Model;
 
         /// <summary>
         /// готов ли юнит принять новый боевой клич
         /// </summary>
-        [SerializeField] private bool _isListen = true; 
+        [SerializeField] private bool _isListen = true;
         public bool IsListen { get => _isListen; set => _isListen = value; }
 
         private void OnEnable()
@@ -31,28 +31,26 @@ namespace Hearthstone
             _card_Model = GetComponent<Card_Model>();
             _card_Controller = GetComponent<Card_Controller>();
             _battleCryController = FindObjectOfType<BattleCry_Controller>();
-            _battleModeCard_View = GetComponent<BattleModeCard_View>();          
+            _battleModeCard_View = GetComponent<BattleModeCard_View>();
         }
 
         public void OnPointerClick(PointerEventData eventData)
         {
-            if (transform.parent.gameObject.GetComponent<Board>()
-                && _isListen && _battleCryController.IsActiveCry
+            if (transform.parent.gameObject.GetComponent<Board>() && _isListen && _battleCryController.IsActiveCry
                 && (_battleCryController._battleCryTargets_Active != Target.Self))//определяем цель боевого клича
             {
-                if (_battleCryController.BattleCryCreator != eventData.pointerClick.gameObject) //исключаем применение боевого клича на себя
-                {   
-                    ApplyNewBattleCry();
-                    _isListen = false;
-                    _battleCryController.IsActiveCry = false;                    
-                    _battleCryController.UpdateBattleCry();
+                if (_battleCryController.BattleCryCreator == eventData.pointerClick) return;//исключаем применение боевого клича на себя
+                if (_battleCryController._battleCryTargets_Active == Target.SingleFriend && _battleCryController.BattleCryCreator.GetComponent<Card>().GetSide() != GetComponent<Card>().GetSide()) return;
+                ApplyNewBattleCry();
+                _isListen = false;
+                _battleCryController.IsActiveCry = false;
+                _battleCryController.UpdateBattleCry();
 
-                    //test
-                    IndicatorTarget indicatorTarget = FindObjectOfType<IndicatorTarget>();
-                    indicatorTarget.ChangeCursorState(false);
-                    ///test
+                //test
+                IndicatorTarget indicatorTarget = FindObjectOfType<IndicatorTarget>();
+                indicatorTarget.ChangeCursorState(false);
+                ///test
 
-                }                
             }
         }
 
@@ -61,41 +59,41 @@ namespace Hearthstone
         {
             foreach (AbilityCurrentCard abilityInTarget in _battleCryController._curentAbilityInTarget)
             {
-                if((_battleCryController._battleCryTargetsType_Active == _card_Model._minionType) || _battleCryController._battleCryTargetsType_Active == MinionType.None )
-                {      
-                    if(abilityInTarget == AbilityCurrentCard.Provocation)
+                if ((_battleCryController._battleCryTargetsType_Active == _card_Model._minionType) || _battleCryController._battleCryTargetsType_Active == MinionType.None)
+                {
+                    if (abilityInTarget == AbilityCurrentCard.Provocation)
                     {
-                        _card_Controller.ProvocationAbility(true);                        
+                        _card_Controller.ProvocationAbility(true);
                     }
 
 
                     //изменяем атаку
-                    if (abilityInTarget == AbilityCurrentCard.ChangeAtack) 
+                    if (abilityInTarget == AbilityCurrentCard.ChangeAtack)
                     {
                         _card_Controller.ChangeAtackValue(_battleCryController.BattleCryChangeAtackDamage);
                     }
                     //изменяем здоровье
-                    if (abilityInTarget == AbilityCurrentCard.ChangeHealt) 
+                    if (abilityInTarget == AbilityCurrentCard.ChangeHealt)
                     {
-                        foreach(BattleCryType cryType in _battleCryController._currentBattleCryTypes)
+                        foreach (BattleCryType cryType in _battleCryController._currentBattleCryTypes)
                         {
                             if (cryType == BattleCryType.DealDamage) //принимаем урон
                             {
-                                
+
                                 _eventEffectController.ParseDamageEvent(this);
                                 _card_Controller.ChangeHealthValue(_battleCryController.BattleCryChangeHealth, ChangeHealthType.DealDamage);
                                 _singleEffectController.ApplyEffect(this); //переделать в дженерики
                                 /*if (_card_Model._healthCard <= 0)
                                     _card_Controller.DiedCreature(); //событие смерти*/
-                            }                                
+                            }
 
                             if (cryType == BattleCryType.Heal)//лечим себя
                             {
                                 if (_card_Model.HealthCard < _card_Model.MaxHealtValue)
                                 {
-                                    _eventEffectController.ParseHealEvent(this);                                    
-                                }                               
-                                _card_Controller.ChangeHealthValue(_battleCryController.BattleCryChangeHealth, ChangeHealthType.Healing);                                
+                                    _eventEffectController.ParseHealEvent(this);
+                                }
+                                _card_Controller.ChangeHealthValue(_battleCryController.BattleCryChangeHealth, ChangeHealthType.Healing);
                                 if (_card_Model.HealthCard > _card_Model.MaxHealtValue)
                                     _card_Model.HealthCard = _card_Model.MaxHealtValue;
 
@@ -104,16 +102,16 @@ namespace Hearthstone
                                 _battleModeCard_View.UpdateViewCard();
                                 StartCoroutine(_battleModeCard_View.EffectParticle(_battleModeCard_View.HealtEffect));
                             }
-                            if(cryType == BattleCryType.RaiseParametrs) //повышаем параметры
+                            if (cryType == BattleCryType.RaiseParametrs) //повышаем параметры
                             {
                                 _card_Model.MaxHealtValue += _battleCryController.BattleCryChangeHealth;
-                                _card_Controller.ChangeHealthValue(_battleCryController.BattleCryChangeHealth,ChangeHealthType.RaiseParametrs);                                
+                                _card_Controller.ChangeHealthValue(_battleCryController.BattleCryChangeHealth, ChangeHealthType.RaiseParametrs);
                                 _battleModeCard_View.UpdateViewCard();
                             }
-                        } 
+                        }
                     }
-                }               
+                }
             }
-        }      
+        }
     }
 }
